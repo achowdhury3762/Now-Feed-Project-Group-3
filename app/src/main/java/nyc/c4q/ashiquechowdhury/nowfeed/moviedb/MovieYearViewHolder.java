2337fc999809bdbb123f2c5951439a42996c4134
achowdhury3762 <achowdhury3762@gmail.com>
2017-01-06 15:54:51 -0500

    Latest changes

--- app/src/main/java/nyc/c4q/ashiquechowdhury/nowfeed/moviedb/MovieYearViewHolder.java
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/nowfeed/moviedb/MovieYearViewHolder.java
@@ -5,8 +5,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
-
 import nyc.c4q.ashiquechowdhury.nowfeed.R;
 
 /**
@@ -28,6 +26,5 @@ public class MovieYearViewHolder extends RecyclerView.ViewHolder{
         if( movieDescriptions.getDescription().length() == 0){
             movieDescription.setText("No Overview Found");
         }
-        Picasso.with(itemView.getContext()).load("http://image.tmdb.org/t/p/w300" + movieDescriptions.getImagePath()).fit().into(movieImage);
     }
 }
--- app/src/main/java/nyc/c4q/ashiquechowdhury/nowfeed/workout/ProgressActivity.java
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/nowfeed/workout/ProgressActivity.java
@@ -1,10 +1,12 @@
 package nyc.c4q.ashiquechowdhury.nowfeed.workout;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import nyc.c4q.ashiquechowdhury.nowfeed.R;
 
@@ -13,14 +15,18 @@ import nyc.c4q.ashiquechowdhury.nowfeed.R;
  */
 public class ProgressActivity extends AppCompatActivity {
     EditText weightTextView;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.progress_layout);
-
         weightTextView = (EditText) findViewById(R.id.weightedittext);
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs = getPreferences(Context.MODE_PRIVATE);
+    }
+
+    public void onClickSubmit(View view){
+        Toast.makeText(this, "Saved Weight!", Toast.LENGTH_LONG).show();
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString("WEIGHT", weightTextView.getText().toString());
         editor.apply();
